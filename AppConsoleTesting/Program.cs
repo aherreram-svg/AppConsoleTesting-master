73b4fecda0dd810f2edfcc6831aa3737c12b90b9
Alberto Herrera Medina <aherreram@unincca.edu.co>
2026-02-08 22:56:52 -0500

    Se agrega un console con mi nombre

--- AppConsoleTesting/Program.cs
+++ AppConsoleTesting/Program.cs
@@ -7,6 +7,7 @@ Console.WriteLine("Esto es una prueba de git v1");
 Console.WriteLine("Esto es una prueba de git v2");
 
 Console.WriteLine("Esto es una prueba Alberto Herrera ");
+Console.WriteLine("Esto es una prueba 4 Alberto Herrera ");
 
 Console.WriteLine("Esto es una prueba 2 Alberto Herrera ");
 Console.WriteLine("Esto es una prueba 3 Alberto Herrera ");
